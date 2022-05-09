406462a93780a83875f0cc34ff04156a6e8c0c60
Bogdan Lukin <lukin.bogdan.a@gmail.com>
2022-05-10 01:05:58 +0700

    add basic navigation with built-in about dialog

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:inno_example/pages/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigation App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static const title = "Home";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(title),
+        actions: [
+          IconButton(
+              onPressed: () => showAboutDialog(context: context),
+              icon: const Icon(Icons.account_balance_outlined)),
+        ],
+      ),
+      body: Row(
+        children: [],
+      ),
+    );
+  }
+}
